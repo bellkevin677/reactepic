c36808c1d3878e99ce7f2ee3c483e9a6c43edcdf
Kevin Bell <bellkevin677@gmail.com>
2021-06-01 09:31:22 -0500

    Ran Npm install and npm install gh pages

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const root = document.getElementById('root');
 
 SMART.init({
   iss: "https://fhir.epic.com/interconnect-fhir-oauth/api/FHIR/DSTU2/",
-  redirectUri: "https://bellkevin677.github.io/",
+  redirectUri: "http://bellkevin677.github.io/reactepic/",
   clientId: "593798c6-39ad-4ff6-813a-7cc4fd0e74d4",
   scope: "launch/patient offline_access openid fhirUser",
 }).then(client => {
