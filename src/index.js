48cdb5d1ae26f3ff7d3b8546cdf5c534460551a7
Devon <devon.mobley@gmail.com>
2021-07-06 10:07:18 -0500

    working medication pull

--- src/index.js
+++ src/index.js
@@ -7,66 +7,63 @@ import reportWebVitals from './reportWebVitals';
 
 const root = document.getElementById('root');
 
-
 console.log(client);
 
 SMART.init({
-  iss: "https://fhir.epic.com/interconnect-fhir-oauth/api/FHIR/R4/",
-  redirectUri: "http://localhost:3000",
-  clientId: "593798c6-39ad-4ff6-813a-7cc4fd0e74d4",
-  scope: "openid fhirUser patient/*.* MedicationStatement/*.* MedicationRequest/*.* Medication/*.* RelatedPerson/*.* online_access",
+  iss: 'https://fhir.epic.com/interconnect-fhir-oauth/api/FHIR/R4/',
+  redirectUri: 'http://localhost:3000',
+  clientId: '593798c6-39ad-4ff6-813a-7cc4fd0e74d4',
+  scope:
+    'openid fhirUser patient/*.* MedicationStatement/*.* MedicationRequest/*.* Medication/*.* RelatedPerson/*.* online_access',
 })
 
-// Condition.Search FamilyMemberHistory.Search RelatedPerson.Read
-  
+  // Condition.Search FamilyMemberHistory.Search RelatedPerson.Read
 
-.then(async (client) => {
-  const patient = await client.patient.read()
-  console.log (patient)
-  console.log (client)
+  .then(
+    async (client) => {
+      const patient = await client.patient.read();
+      console.log(patient);
+      console.log(client);
 
-  // const medicationStatement = await client.request(`/MedicationStatement?patient=${patient.id}`)
-  // console.log ("medicationStatement", medicationStatement)
+      // const medicationStatement = await client.request(`/MedicationStatement?patient=${patient.id}`)
+      // console.log ("medicationStatement", medicationStatement)
 
-  // const related = await client.request(`/RelatedPerson?patient=${patient.id}`)
-  // console.log ("related", related)
+      // const related = await client.request(`/RelatedPerson?patient=${patient.id}`)
+      // console.log ("related", related)
 
-  const medication = await client.request(`/Medication?patient=${patient.id}`)
-  console.log ("medication", medication)
+      const medication = await client.request(
+        `/MedicationRequest?patient=${patient.id}`
+      );
+      console.log('medication', medication);
 
-  // const condition = await client.request(`STU3/Condition?subject={subject}&patient=${patient.id}&clinical-status={clinical-status}&category={category}&encounter={encounter}`)
-  // console.log ("condition", condition)
-  
-  // const famHistory = await client.request(`/FamilyMemberHistory?patient={patient.id}&subject={subject}`)
-  // console.log ("famHistory", famHistory)
+      // const condition = await client.request(`STU3/Condition?subject={subject}&patient=${patient.id}&clinical-status={clinical-status}&category={category}&encounter={encounter}`)
+      // console.log ("condition", condition)
 
+      // const famHistory = await client.request(`/FamilyMemberHistory?patient={patient.id}&subject={subject}`)
+      // console.log ("famHistory", famHistory)
 
-
-  ReactDOM.render(
-    <React.StrictMode>
-      <App 
-        Patient={patient}
-        // medication={medication}
-        // condition={condition}
-        // related={related}
-        // famHistory={famHistory}
-      />
-    </React.StrictMode>
-  , root);
-}, err => {
-  ReactDOM.render(
-    <React.StrictMode>
-      <App 
-        Error={err}
-      />
-    </React.StrictMode>
-  , root);
-});
-
-
-
-
-
+      ReactDOM.render(
+        <React.StrictMode>
+          <App
+            Patient={patient}
+            // medication={medication}
+            // condition={condition}
+            // related={related}
+            // famHistory={famHistory}
+          />
+        </React.StrictMode>,
+        root
+      );
+    },
+    (err) => {
+      ReactDOM.render(
+        <React.StrictMode>
+          <App Error={err} />
+        </React.StrictMode>,
+        root
+      );
+    }
+  );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
