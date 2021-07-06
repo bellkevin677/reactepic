e7664849964b4944f6b130135cc7c00f81b92ee7
Devon <devon.mobley@gmail.com>
2021-07-06 14:39:36 -0500

    changing things up a bit - cleaning up variable names and App props.

--- src/App.js
+++ src/App.js
@@ -1,87 +1,86 @@
 import React from 'react';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Button from 'react-bootstrap/Button';
-import {
-  Table,
-  Card,
-  ListGroup
-} from 'react-bootstrap';
+import { Table, Card, ListGroup } from 'react-bootstrap';
 
 export default class App extends React.Component {
   render() {
-
     const {
       Error,
       Client,
-      Patient,
+      patient,
       Data,
-      medicationStatement,
+      medications,
       // condition,
       // related,
       // famHistory
     } = this.props;
 
+    let name =
+      patient.name.find((arrayRecord) => arrayRecord.use === 'official') ||
+      patient.name;
 
-
-
-    let Name =
-      Patient.name.find((arrayRecord) => arrayRecord.use === 'official') ||
-      Patient.name;
-
-    let ID =
-      Patient.identifier.find(
+    let identifier =
+      patient.identifier.find(
         (arrayRecord) => arrayRecord.use === 'official'
-      ) || Patient.identifier[0];
+      ) || patient.identifier[0];
 
     let Address =
-      Patient.address.find((arrayRecord) => arrayRecord.use === 'official') ||
-      Patient.address[0];
+      patient.address.find((arrayRecord) => arrayRecord.use === 'official') ||
+      patient.address[0];
 
     // Code from Andrew
     // let Medname =
     // medicationRequest.entry[0].resource;
     // console.log(Medname);
 
-
-    return <div >
-      <h1>My Health Access</h1>
-      <br></br>
-      <div class="Card">
-        <Card style={{ width: '500px' }} className="d-flex justify-content-center align-items-center" border="primary">
-          <Table striped bordered hover size="sm" bg="light">
-            <thead>
-              <tr>
-                <th >Patient Info</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td >Patient ID:</td>
-                <td>{ID.value || "n/a"}</td>
-              </tr>
-              <tr>
-                <td>Patient Name:</td>
-                <td>{Name.text || "n/a"}</td>
-              </tr>
-              <tr>
-                <td>Gender:</td>
-                <td> {Patient.gender || "n/a"}</td>
-              </tr>
-              <tr>
-                <td>Birthday:</td>
-                <td> {Patient.birthDate || "n/a"}</td>
-              </tr>
-              <tr>
-                <td>Address:</td>
-                <td> {Address.line[0] || "n/a"}, {Address.city}, {Address.state}, {Address.postalCode}</td>
-              </tr>
-            </tbody>
-          </Table>
-        </Card>
+    return (
+      <div>
+        <h1>My Health Access</h1>
+        <br></br>
+        <div class="Card">
+          <Card
+            style={{ width: '500px' }}
+            className="d-flex justify-content-center align-items-center"
+            border="primary"
+          >
+            <Table striped bordered hover size="sm" bg="light">
+              <thead>
+                <tr>
+                  <th>Patient Info</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Patient ID:</td>
+                  <td>{identifier.value || 'n/a'}</td>
+                </tr>
+                <tr>
+                  <td>Patient Name:</td>
+                  <td>{name.text || 'n/a'}</td>
+                </tr>
+                <tr>
+                  <td>Gender:</td>
+                  <td> {patient.gender || 'n/a'}</td>
+                </tr>
+                <tr>
+                  <td>Birthday:</td>
+                  <td> {patient.birthDate || 'n/a'}</td>
+                </tr>
+                <tr>
+                  <td>Address:</td>
+                  <td>
+                    {Address.line[0] || 'n/a'}, {Address.city}, {Address.state},{' '}
+                    {Address.postalCode}
+                  </td>
+                </tr>
+              </tbody>
+            </Table>
+          </Card>
+        </div>
       </div>
-    </div >
+    );
   }
 }
-
